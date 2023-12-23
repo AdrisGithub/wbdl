8ebe65a3c3eae90b96b1871df09f8a369700a031
Adrian John <adrianjohn6543@gmail.com>
2023-12-23 21:35:43 +0100

    change the exports

--- src/main/lib.rs
+++ src/main/lib.rs
@@ -1,5 +1,14 @@
-pub mod date;
-pub mod error;
-pub mod month;
+pub use date::Date;
+pub use error::WBDLError;
+pub use month::Month;
+pub use month::Season;
+pub use time::Day;
+pub use time::Hour;
+pub use time::Minute;
+pub use time::Second;
+
+mod date;
+mod error;
+mod month;
+mod time;
 pub mod util;
-pub mod time;
